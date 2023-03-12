01dbd012513676037810596f4aa92ec8f9a22632
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-03-12 18:58:08 +0200

    Tenders page completed

--- inno-bid/src/home/Home.jsx
+++ inno-bid/src/home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Topbar from "../topbar/Topbar"
-import Sidebar from '../sidebar/Sidebar'
 import Dashboard from "../dashboard/Dashboard";
 import "./home.css"
+import Sidebar2 from '../sidebar/Sidebar2';
 
 export default function Home() {
   return (
     <>
     <Topbar/>
     <div className='home'>
-      <Sidebar />
+      <Sidebar2 />
       <Dashboard/>
         
 
